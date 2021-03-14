2f92b8c8450d552ec62b9432a4873bf1010c52a5
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 21:35:32 +0100

    Added testcases for StandardBattleShipCellVerifier

---
+++ CoreTests/Services/StandardBattleShipCellVerifierTest.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Core.Model;
+using Core.Services;
+using CoreTests.TestData.Services.CellVerifier;
+using CoreTests.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace CoreTests.Services
+{
+    public class CellVerifierTest
+    {
+        private readonly StandardBattleShipCellVerifier _sut;
+
+
+        public CellVerifierTest()
+        {
+            _sut = new StandardBattleShipCellVerifier();
+        }
+
+        [Theory(DisplayName = "Verifier returns true if cells are adjacent and on the same axis")]
+        [ClassData(typeof(ValidCellsClassData))]
+        public void Verifier_returns_true_if_cells_are_adjacent_and_on_the_same_axis(IEnumerable<Cell> cells)
+        {
+            var isValid = _sut.Verify(cells);
+
+            isValid.Should().BeTrue("All of provided Cell collections contain Cells that are adjacent and on the same axis");
+        }
+
+        [Theory(DisplayName = "Verifier returns false if cells are not adjacent or not on the same axis")]
+        [ClassData(typeof(InvalidCellsClassData))]
+        public void Verifier_returns_false_if_cells_are_not_adjacent_or_not_on_the_same_axis(IEnumerable<Cell> cells)
+        {
+            var isValid = _sut.Verify(cells);
+
+            isValid.Should().BeFalse("All of provided Cell collections contain Cells that are not adjacent or not on the same axis");
+        }
+    }
+}
