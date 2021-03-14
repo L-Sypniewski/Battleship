93234671a1ee4e1342683835f4f8f34cebc3fc9b
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 16:33:46 +0100

    BoardInitializer adds correct amount of ships to a Board

--- Core/Services/BoardInitializer.cs
+++ Core/Services/BoardInitializer.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Core.Model;
 
 namespace Core.Services
 {
     public class BoardInitializer : IBoardInitializer
     {
-        public Board InitializedBoard(Board board, IEnumerable<Ship> ships) => throw new System.NotImplementedException();
+        public Board InitializedBoard(BoardSize boardSize, ISet<ShipConfiguration> shipConfigurations)
+        {
+            var shipsNumber = shipConfigurations.Sum(x => x.ShipsNumber);
+
+            var emptyShip = new Ship("", Array.Empty<Cell>());
+            return new Board(boardSize, Enumerable.Repeat(emptyShip, shipsNumber).ToArray());
+        }
     }
 }
