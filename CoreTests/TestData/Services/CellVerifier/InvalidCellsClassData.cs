31afc9d0c1446c3774115518d403f755e3273b5b
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 22:08:58 +0100

    Implemented StandardBattleShipCellVerifier

--- CoreTests/TestData/Services/CellVerifier/InvalidCellsClassData.cs
+++ CoreTests/TestData/Services/CellVerifier/InvalidCellsClassData.cs
@@ -10,38 +10,66 @@ namespace CoreTests.TestData.Services.CellVerifier
         {
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(1, 1).Build(),
-                new CellBuilder().WithCoordinates(0, 2).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(1, 1).Build(),
+                    new CellBuilder().WithCoordinates(0, 2).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(0, 2).Build(),
-                new CellBuilder().WithCoordinates(0, 3).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(0, 2).Build(),
+                    new CellBuilder().WithCoordinates(0, 3).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(1, 0).Build(),
-                new CellBuilder().WithCoordinates(3, 0).Build(),
-                new CellBuilder().WithCoordinates(4, 0).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(1, 0).Build(),
+                    new CellBuilder().WithCoordinates(3, 0).Build(),
+                    new CellBuilder().WithCoordinates(4, 0).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(1, 0).Build(),
-                new CellBuilder().WithCoordinates(2, 1).Build(),
-                new CellBuilder().WithCoordinates(3, 2).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(1, 0).Build(),
+                    new CellBuilder().WithCoordinates(2, 1).Build(),
+                    new CellBuilder().WithCoordinates(3, 2).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(24, 5).Build(),
-                new CellBuilder().WithCoordinates(25, 6).Build(),
-                new CellBuilder().WithCoordinates(24, 7).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(24, 5).Build(),
+                    new CellBuilder().WithCoordinates(25, 6).Build(),
+                    new CellBuilder().WithCoordinates(24, 7).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(0, 2).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(0, 2).Build()
+                }
+            };
+            yield return new object[]
+            {
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(2, 120).Build(),
+                    new CellBuilder().WithCoordinates(2, 121).Build(),
+                    new CellBuilder().WithCoordinates(2, 122).Build(),
+                    new CellBuilder().WithCoordinates(2, 124).Build()
+                }
             };
         }
 
