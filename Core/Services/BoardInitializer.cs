48a15e95bc300c3ac0d6604dfc3cabc330857692
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 15:08:54 +0100

    Added BoardInitializer stub

---
+++ Core/Services/BoardInitializer.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Core.Model;
+
+namespace Core.Services
+{
+    public class BoardInitializer : IBoardInitializer
+    {
+        public Board InitializedBoard(Board board, IEnumerable<Ship> ships) => throw new System.NotImplementedException();
+    }
+}
