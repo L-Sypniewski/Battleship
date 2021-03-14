fcc6dada07b2d2d85d5117cfc0a558bdeed78fa3
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 21:01:13 +0100

    Added separate classes for test data for BoardVerifierTests

--- CoreTests/Services/BoardVerifierTest.cs
+++ CoreTests/Services/BoardVerifierTest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Core.Exceptions;
 using Core.Model;
 using Core.Services;
-using CoreTests.Utils;
+using CoreTests.TestData.Services.BoardVerifier;
 using FluentAssertions;
 using Xunit;
 
@@ -23,7 +23,7 @@ namespace CoreTests.Services
 
 
         [Theory(DisplayName = "BoardVerifier correctly detects when some Ships overlap")]
-        [MemberData(nameof(OverlappingShipsTestData))]
+        [ClassData(typeof(OverlappingShipsClassData))]
         public void BoardVerifier_correctly_detects_when_some_Ships_overlap(IEnumerable<Ship> ships)
         {
             BoardSize boardSize = new(0, 0);
@@ -35,55 +35,8 @@ namespace CoreTests.Services
         }
 
 
-        public static IEnumerable<object[]> OverlappingShipsTestData()
-        {
-            return new List<object[]>
-            {
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(0, 0).Build(),
-                            new CellBuilder().WithCoordinates(0, 1).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(0, 0).Build(),
-                            new CellBuilder().WithCoordinates(1, 0).Build()
-                        }).Build()
-                    }
-                },
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(25, 2).Build(),
-                            new CellBuilder().WithCoordinates(25, 3).Build(),
-                            new CellBuilder().WithCoordinates(26, 3).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(0, 0).Build(),
-                            new CellBuilder().WithCoordinates(1, 0).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(26, 5).Build(),
-                            new CellBuilder().WithCoordinates(26, 4).Build(),
-                            new CellBuilder().WithCoordinates(26, 3).Build()
-                        }).Build()
-                    }
-                }
-            };
-        }
-
-
         [Theory(DisplayName = "BoardVerifier correctly detects when Ships don't overlap")]
-        [MemberData(nameof(NonOverlappingShipsTestData))]
+        [ClassData(typeof(NonOverlappingShipsClassData))]
         public void BoardVerifier_correctly_detects_when_Ships_do_not_overlap(IEnumerable<Ship> ships)
         {
             BoardSize boardSize = new(0, 0);
@@ -95,55 +48,8 @@ namespace CoreTests.Services
         }
 
 
-        public static IEnumerable<object[]> NonOverlappingShipsTestData()
-        {
-            return new List<object[]>
-            {
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(0, 0).Build(),
-                            new CellBuilder().WithCoordinates(0, 1).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(2, 0).Build(),
-                            new CellBuilder().WithCoordinates(2, 1).Build()
-                        }).Build()
-                    }
-                },
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(100, 1).Build(),
-                            new CellBuilder().WithCoordinates(100, 2).Build(),
-                            new CellBuilder().WithCoordinates(101, 2).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(0, 0).Build(),
-                            new CellBuilder().WithCoordinates(1, 0).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(26, 5).Build(),
-                            new CellBuilder().WithCoordinates(26, 4).Build(),
-                            new CellBuilder().WithCoordinates(26, 3).Build()
-                        }).Build()
-                    }
-                }
-            };
-        }
-
-
         [Theory(DisplayName = "BoardVerifier throws exception if any of the Ships has negative coordinates")]
-        [MemberData(nameof(ShipsWithNegativeCoordinates))]
+        [ClassData(typeof(ShipsWithNegativeCoordinatesClassData))]
         public void BoardVerifier_throws_exception_if_any_of_the_Ships_has_negative_coordinates(IEnumerable<Ship> allShips,
             IEnumerable<Ship> invalidShips)
         {
@@ -160,231 +66,12 @@ namespace CoreTests.Services
         }
 
 
-        public static IEnumerable<object[]> ShipsWithNegativeCoordinates()
-        {
-            return new List<object[]>
-            {
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, 1).Build(),
-                            new CellBuilder().WithCoordinates(25, 1).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-3, 4).Build()
-                        }).Build()
-                    },
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, 1).Build(),
-                            new CellBuilder().WithCoordinates(25, 1).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-3, 4).Build()
-                        }).Build()
-                    }
-                },
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(15, 15).Build(),
-                            new CellBuilder().WithCoordinates(25, 1).Build()
-                        }).Build(),
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(13, -6).Build()
-                        }).Build()
-                    },
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(13, -6).Build()
-                        }).Build()
-                    }
-                },
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, 1).Build()
-                        }).Build()
-                    },
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, 1).Build()
-                        }).Build()
-                    }
-                },
-                new object[]
-                {
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, -1).Build()
-                        }).Build()
-                    },
-                    new[]
-                    {
-                        new ShipBuilder().WithCells(new[]
-                        {
-                            new CellBuilder().WithCoordinates(-100, -1).Build()
-                        }).Build()
-                    }
-                }
-            };
-        }
-
-
         [Theory(DisplayName = "BoardVerifier correctly verifies if Ships are within bounds of a Board")]
-        [MemberData(nameof(ShipsWithinBoardBoundsCoordinates))]
+        [ClassData(typeof(ShipsWithinBoardBoundsCoordinatesClassData))]
         public void BoardVerifier_correctly_throws_exception_if_any_of_the_Ships_has_negative_coordinates(Board board,
             bool expectedResult)
         {
             _sut.ShipsAreWithinBounds(board).Should().Be(expectedResult);
-        }
-
-
-        public static IEnumerable<object[]> ShipsWithinBoardBoundsCoordinates()
-        {
-            return new List<object[]>
-            {
-                new object[]
-                {
-                    new Board(new BoardSize(10, 10),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(0, 0).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(2, 1).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    true
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(100, 75),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(100, 75).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    true
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(75, 100),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(100, 75).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    false
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(10, 10),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(10, 10).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(10, 10).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    true
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(10, 10),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(25, 25).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(25, 25).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    false
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(25, 25),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(25, 25).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(25, 25).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    true
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(10, 10),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(0, 0).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(2, 11).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    false
-                },
-                new object[]
-                {
-                    new Board(new BoardSize(10, 10),
-                              new[]
-                              {
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(100, 0).Build()
-                                  }).Build(),
-                                  new ShipBuilder().WithCells(new[]
-                                  {
-                                      new CellBuilder().WithCoordinates(12, 8).Build()
-                                  }).Build()
-                              }.ToImmutableList()),
-                    false
-                }
-            };
         }
     }
 }
