1f9b8b5bb602c100ecea0efe9287c031f9abc873
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 20:19:46 +0100

    Implemented ShipsAreWithinBounds check in IBoardVerifier

--- Core/Model/Ship.cs
+++ Core/Model/Ship.cs
@@ -1,4 +1,24 @@
-﻿namespace Core.Model
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace Core.Model
 {
-    public sealed record Ship(string Name, Cell[] Cells);
+    public sealed record Ship(string Name, IImmutableList<Cell> Cells)
+    {
+        private bool PrintMembers(StringBuilder builder)
+        {
+            builder.Append($"{nameof(Name)}: {Name},");
+            builder.Append($"{nameof(Cells)}: {string.Join(',', Cells.Select(cell => cell.ToString()))}");
+            return true;
+        }
+
+
+        public bool Equals(Ship? other) => Name.Equals(other?.Name) ||
+                                           Cells.SequenceEqual(other?.Cells ?? new ImmutableArray<Cell>());
+
+
+        public override int GetHashCode() => HashCode.Combine(Name, Cells);
+    }
 }
