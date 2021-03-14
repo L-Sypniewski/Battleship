588f4cae3c27743716a381fd2c5257e4a13b3588
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 16:33:46 +0100

    BoardInitializer adds correct amount of ships to a Board

---
+++ Core/Model/ShipConfiguration.cs
@@ -0,0 +1,15 @@
+using Core.Services;
+
+namespace Core.Model
+{
+    /// <summary>
+    ///     Ship configuration used by <see cref="IBoardInitializer" /> to initialize a board.
+    ///     Two configuraions for the same <see cref="ShipSize" /> are consifered equal
+    /// </summary>
+    public sealed record ShipConfiguration(int ShipSize, int ShipsNumber)
+    {
+        public override int GetHashCode() => ShipSize.GetHashCode();
+
+        public bool Equals(ShipConfiguration? other) => ShipSize.Equals(other?.ShipSize);
+    }
+}
