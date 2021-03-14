149485885913ffad31d140850a09693a1be540ba
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 21:08:36 +0100

    Added CellVerifier stub

---
+++ CoreTests/Services/CellVerifierTest.cs
@@ -0,0 +1,16 @@
+using Core.Services;
+
+namespace CoreTests.Services
+{
+    public class CellVerifierTest
+    {
+        private readonly CellVerifier _sut;
+
+
+        public CellVerifierTest()
+        {
+            _sut = new CellVerifier();
+        }
+
+    }
+}
