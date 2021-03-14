e55fb65c7a7dead2d9987cff1a1e3b9870cb17fa
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 18:02:46 +0100

    Added checking if Ships overlap on a Board
    Implemented BoardVerifier

---
+++ CoreTests/Utils/Builders/CellBuilder.cs
@@ -0,0 +1,38 @@
+using Core.Model;
+
+namespace CoreTests.Utils
+{
+    public class CellBuilder
+    {
+        private bool _isShot;
+        private int _xCoordinate;
+        private int _yCoordinate;
+
+
+        public CellBuilder()
+        {
+            _yCoordinate = 0;
+        }
+
+
+        public CellBuilder WithIsShot(bool isShot)
+        {
+            _isShot = isShot;
+            return this;
+        }
+
+
+        public CellBuilder WithCoordinates(int xCoordinate, int yCoordinate)
+        {
+            _xCoordinate = xCoordinate;
+            _yCoordinate = yCoordinate;
+            return this;
+        }
+
+
+        public Cell Build()
+        {
+            return new Cell(_xCoordinate, _yCoordinate, _isShot);
+        }
+    }
+}
