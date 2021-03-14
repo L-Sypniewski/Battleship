afc7520258ea00c1735e9afbdcfba1adb2e722e4
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 22:08:58 +0100

    Implemented StandardBattleShipCellVerifier

--- CoreTests/TestData/Services/CellVerifier/ValidCellsClassData.cs
+++ CoreTests/TestData/Services/CellVerifier/ValidCellsClassData.cs
@@ -4,59 +4,80 @@ using CoreTests.Utils;
 
 namespace CoreTests.TestData.Services.CellVerifier
 {
-    public class ValidCellsClassData: IEnumerable<object[]>
+    public class ValidCellsClassData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build()
+                new[] {new CellBuilder().WithCoordinates(0, 0).Build()}
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(10, 23).Build()
+                new[] {new CellBuilder().WithCoordinates(10, 23).Build()}
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(0, 1).Build(),
-                new CellBuilder().WithCoordinates(0, 2).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(0, 1).Build(),
+                    new CellBuilder().WithCoordinates(0, 2).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(0, 1).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(0, 1).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(0, 0).Build(),
-                new CellBuilder().WithCoordinates(0, 2).Build(),
-                new CellBuilder().WithCoordinates(0, 1).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(0, 2).Build(),
+                    new CellBuilder().WithCoordinates(0, 1).Build()
+                }
             };
             yield return new[]
             {
-                new CellBuilder().WithCoordinates(1, 0).Build(),
-                new CellBuilder().WithCoordinates(2, 0).Build(),
-                new CellBuilder().WithCoordinates(3, 0).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(1, 0).Build(),
+                    new CellBuilder().WithCoordinates(2, 0).Build(),
+                    new CellBuilder().WithCoordinates(3, 0).Build()
+                }
             };
             yield return new[]
             {
-                new CellBuilder().WithCoordinates(2, 0).Build(),
-                new CellBuilder().WithCoordinates(1, 0).Build(),
-                new CellBuilder().WithCoordinates(3, 0).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(2, 0).Build(),
+                    new CellBuilder().WithCoordinates(1, 0).Build(),
+                    new CellBuilder().WithCoordinates(3, 0).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(24, 5).Build(),
-                new CellBuilder().WithCoordinates(24, 6).Build(),
-                new CellBuilder().WithCoordinates(24, 7).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(24, 5).Build(),
+                    new CellBuilder().WithCoordinates(24, 6).Build(),
+                    new CellBuilder().WithCoordinates(24, 7).Build()
+                }
             };
             yield return new object[]
             {
-                new CellBuilder().WithCoordinates(2, 120).Build(),
-                new CellBuilder().WithCoordinates(2, 121).Build(),
-                new CellBuilder().WithCoordinates(2, 122).Build(),
-                new CellBuilder().WithCoordinates(2, 124).Build()
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(2, 120).Build(),
+                    new CellBuilder().WithCoordinates(2, 121).Build(),
+                    new CellBuilder().WithCoordinates(2, 122).Build(),
+                    new CellBuilder().WithCoordinates(2, 123).Build()
+                }
             };
         }
 
