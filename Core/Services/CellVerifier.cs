1c14397c7d6121acf96d748faaa599e58ff20b82
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-14 21:08:36 +0100

    Added CellVerifier stub

---
+++ Core/Services/CellVerifier.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class CellVerifier : ICellVerifier
+    {
+        public bool AllCellsAdjacent(IEnumerable<Cell> cells) => throw new System.NotImplementedException();
+    }
+}
