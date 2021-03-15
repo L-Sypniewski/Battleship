986509889877373e8a98664abcca0fad45898c8c
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 21:40:49 +0100

    Exception is thrown if trying to ShootAt out of bounds Cell

---
+++ Core/Exceptions/CannotMakeOutOfBoundsShotException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class CannotMakeOutOfBoundsShotException : Exception
+    {
+        public CannotMakeOutOfBoundsShotException()
+        {
+        }
+
+
+        public CannotMakeOutOfBoundsShotException(string message)
+            : base(message)
+        {
+        }
+
+
+        public CannotMakeOutOfBoundsShotException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
