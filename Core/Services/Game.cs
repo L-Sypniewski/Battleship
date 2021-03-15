0820225ddb6b42132482989801e263c53399d935
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 19:34:54 +0100

    Game - implemented starting new game

--- Core/Services/Game.cs
+++ Core/Services/Game.cs
@@ -1,10 +1,29 @@
-﻿using Core.Model;
+﻿using System.Collections.Generic;
+using Core.Model;
 
 namespace Core.Services
 {
     public sealed class Game : IGame
     {
-        public Board StartGame() => throw new System.NotImplementedException();
+        private readonly BoardSize _boardSize;
+        private readonly ISet<ShipConfiguration> _shipConfigurations;
+        private readonly IBoardInitializer _boardInitializer;
+
+
+        public Game(BoardSize boardSize,
+                    ISet<ShipConfiguration> shipConfigurations,
+                    IBoardInitializer boardInitializer)
+        {
+            _boardSize = boardSize;
+            _shipConfigurations = shipConfigurations;
+            _boardInitializer = boardInitializer;
+        }
+
+
+        public Board StartGame()
+        {
+            return _boardInitializer.InitializedBoard(_boardSize, _shipConfigurations);
+        }
 
 
         public void EndGame() => throw new System.NotImplementedException();
