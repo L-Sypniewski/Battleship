a5d70dd803b3d1020afb4130fa0388b43823d90d
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 15:44:37 +0100

    Implemented BoardInitializer

--- Core/Services/Interfaces/IShipPositioner.cs
+++ Core/Services/Interfaces/IShipPositioner.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using Core.Exceptions;
 using Core.Model;
 
 namespace Core.Services
 {
     public interface IShipPositioner
     {
+        /// <exception cref="CannotCreateShipPositionsException"></exception>
         IReadOnlyCollection<Cell> ShipPositionsFor(Board board, int shipSize);
     }
 }
