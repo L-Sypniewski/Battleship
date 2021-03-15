14e5bd020f66c7f0aa5e6a559c9cdc3bea3c97be
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 10:25:47 +0100

    Implemented ShipPositioner - placing Ships horizontally

---
+++ CoreTests/Services/ShipPositionerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Core.Model;
+using Core.Services;
+using CoreTests.TestData.Services.CellVerifier;
+using CoreTests.TestData.Services.ShipPositioner;
+using CoreTests.Utils;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CoreTests.Services
+{
+    public class ShipPositionerTest
+    {
+        private readonly ShipPositioner _sut;
+
+        private readonly Mock<IShipOrientationRandomizer> _shipOrientationRandomizer = new Mock<IShipOrientationRandomizer>();
+        private readonly Mock<ICellRandomizer> _cellRandomizer = new Mock<ICellRandomizer>();
+        private readonly Mock<IBoardVerifier> _boardVerifier = new Mock<IBoardVerifier>();
+
+
+        public ShipPositionerTest()
+        {
+            _sut = new ShipPositioner(_shipOrientationRandomizer.Object, _cellRandomizer.Object, _boardVerifier.Object);
+        }
+
+
+        [Theory(DisplayName = "Ship are correctly placed in horizontal position")]
+        [ClassData(typeof(PositiningShipsHorizontallyClassData))]
+        public void Ship_are_correctly_placed_in_horizontal_position(Cell firstCell, int shipSize, Cell[] expectedCells)
+        {
+            _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
+                           .Returns(firstCell);
+            _boardVerifier.Setup(mock => mock.ShipsAreWithinBounds(It.IsAny<Board>()))
+                          .Returns(true);
+            _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
+                                      .Returns(ShipOrientation.Horizontal);
+
+            var shipCells = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize);
+            
+            
+            shipCells.Should().BeEquivalentTo(expectedCells);
+        }
+    }
+}
---
+++ CoreTests/TestData/Services/ShipPositioner/PositiningShipsHorizontallyClassData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CoreTests.Utils;
+
+namespace CoreTests.TestData.Services.ShipPositioner
+{
+    public class PositiningShipsHorizontallyClassData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new CellBuilder().WithCoordinates(0, 0).Build(),
+                1,
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                }
+            };
+
+            yield return new object[]
+            {
+                new CellBuilder().WithCoordinates(0, 0).Build(),
+                2,
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(0, 0).Build(),
+                    new CellBuilder().WithCoordinates(1, 0).Build(),
+                }
+            };
+            yield return new object[]
+            {
+                new CellBuilder().WithCoordinates(4, 6).Build(),
+                3,
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(4, 6).Build(),
+                    new CellBuilder().WithCoordinates(5, 6).Build(),
+                    new CellBuilder().WithCoordinates(6, 6).Build(),
+                }
+            };
+            yield return new object[]
+            {
+                new CellBuilder().WithCoordinates(-10, -5).Build(),
+                4,
+                new[]
+                {
+                    new CellBuilder().WithCoordinates(-10, -5).Build(),
+                    new CellBuilder().WithCoordinates(-9, -5).Build(),
+                    new CellBuilder().WithCoordinates(-8, -5).Build(),
+                    new CellBuilder().WithCoordinates(-7, -5).Build(),
+                }
+            };
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
