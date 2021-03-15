dffb500ab3d9961d90088a3d41be2c00ebf1d268
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 13:23:57 +0100

    Changed ShipConfiguration equality check implementation

--- CoreTests/Model/ShipConfigurationTest.cs
+++ CoreTests/Model/ShipConfigurationTest.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Core.Model;
+using CoreTests.Utils;
 using FluentAssertions;
 using Xunit;
 
@@ -8,9 +9,10 @@ namespace CoreTests.Model
 {
     public class ShipConfigurationTest
     {
-        [Theory(DisplayName = "Two configurations with the same ship size are considered to be equal")]
+        [Theory(DisplayName = "Two configurations with the same name nad ship size are considered to be equal")]
         [MemberData(nameof(EqualShipConfigurationsData))]
-        public void Two_configurations_with_the_same_ship_size_are_considered_to_be_equal(ShipConfiguration[] shipConfigurations)
+        public void Two_configurations_with_the_same_name_and_ship_size_are_considered_to_be_equal(
+            ShipConfiguration[] shipConfigurations)
         {
             var referenceObject = shipConfigurations.First();
 
@@ -24,24 +26,39 @@ namespace CoreTests.Model
             {
                 new object[]
                 {
-                    new[] {new ShipConfiguration(1, 1), new ShipConfiguration(1, 2)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithName("myName").WithShipSize(1).WithShipsNumber(1).Build(),
+                        new ShipConfigurationBuilder().WithName("myName").WithShipSize(1).WithShipsNumber(2).Build()
+                    }
                 },
                 new object[]
                 {
-                    new[] {new ShipConfiguration(int.MaxValue, int.MaxValue), new ShipConfiguration(int.MaxValue, int.MinValue)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithName("myAnotherName").WithShipSize(int.MaxValue)
+                                                      .WithShipsNumber(int.MaxValue).Build(),
+                        new ShipConfigurationBuilder().WithName("myAnotherName").WithShipSize(int.MaxValue)
+                                                      .WithShipsNumber(int.MinValue).Build()
+                    }
                 },
                 new object[]
                 {
-                    new[] {new ShipConfiguration(5, 2), new ShipConfiguration(5, -2), new ShipConfiguration(5, 33)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithName("xxx").WithShipSize(5).WithShipsNumber(2).Build(),
+                        new ShipConfigurationBuilder().WithName("xxx").WithShipSize(5).WithShipsNumber(-2).Build(),
+                        new ShipConfigurationBuilder().WithName("xxx").WithShipSize(5).WithShipsNumber(33).Build()
+                    }
                 }
             };
         }
 
 
         [Theory(DisplayName =
-            "Two configurations with the same ships number, but different ship size are NOT considered to be equal")]
+            "Two configurations with the same ships number, but different ship size or name are NOT considered to be equal")]
         [MemberData(nameof(UnequalShipConfigurationsData))]
-        public void Two_configurations_with_the_same_ships_number_but_different_ship_size_are_not_considered_to_be_equal(
+        public void Two_configurations_with_the_same_ships_number_but_different_ship_size_or_name_are_not_considered_to_be_equal(
             ShipConfiguration[] shipConfigurations)
         {
             shipConfigurations.Should().OnlyHaveUniqueItems();
@@ -54,15 +71,44 @@ namespace CoreTests.Model
             {
                 new object[]
                 {
-                    new[] {new ShipConfiguration(2, 1), new ShipConfiguration(3, 1)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithShipSize(2).WithShipsNumber(1).Build(),
+                        new ShipConfigurationBuilder().WithShipSize(3).WithShipsNumber(1).Build()
+                    }
                 },
                 new object[]
                 {
-                    new[] {new ShipConfiguration(int.MaxValue, int.MinValue), new ShipConfiguration(int.MinValue, int.MinValue)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithShipSize(int.MaxValue).WithShipsNumber(int.MinValue).Build(),
+                        new ShipConfigurationBuilder().WithShipSize(int.MinValue).WithShipsNumber(int.MinValue).Build()
+                    }
                 },
                 new object[]
                 {
-                    new[] {new ShipConfiguration(5, 2), new ShipConfiguration(-5, 2), new ShipConfiguration(15, 2)}
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithShipSize(5).WithShipsNumber(2).Build(),
+                        new ShipConfigurationBuilder().WithShipSize(-5).WithShipsNumber(2).Build(),
+                        new ShipConfigurationBuilder().WithShipSize(15).WithShipsNumber(2).Build()
+                    }
+                },
+                new object[]
+                {
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithName("xx").WithShipSize(5).WithShipsNumber(2).Build(),
+                        new ShipConfigurationBuilder().WithName("11").WithShipSize(5).WithShipsNumber(2).Build()
+                    }
+                },
+                new object[]
+                {
+                    new[]
+                    {
+                        new ShipConfigurationBuilder().WithName("xx").WithShipSize(5).WithShipsNumber(2).Build(),
+                        new ShipConfigurationBuilder().WithName("11").WithShipSize(10).WithShipsNumber(2).Build()
+                    }
                 }
             };
         }
