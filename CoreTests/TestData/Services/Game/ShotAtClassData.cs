46f2fa369fc86677bf96cce10bd173e0d6eacdb7
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 20:54:35 +0100

    Added a new testcase for ShootAt() in Game()

--- CoreTests/TestData/Services/Game/ShotAtClassData.cs
+++ CoreTests/TestData/Services/Game/ShotAtClassData.cs
@@ -21,20 +21,66 @@ namespace CoreTests.TestData.Services.Game
                                          }).Build()
                     }).Build(),
                 new CellBuilder().WithCoordinates(2, 2).Build(),
-                new GameMoveResult(new BoardBuilder()
-                                   .WithShips(new[]
-                                   {
-                                       new ShipBuilder().WithName("firstShip")
-                                                        .WithCells(new[]
-                                                        {
-                                                            new CellBuilder().WithCoordinates(2, 2).Build()
-                                                        }).Build()
-                                   }).Build(),
-                                   new ShipBuilder().WithName("firstShip")
-                                                    .WithCells(new[]
-                                                    {
-                                                        new CellBuilder().WithCoordinates(2, 2).Build()
-                                                    }).Build())
+                new GameMoveResult(
+                    new BoardBuilder()
+                        .WithShips(new[]
+                        {
+                            new ShipBuilder().WithName("firstShip")
+                                             .WithCells(new[]
+                                             {
+                                                 new CellBuilder().WithCoordinates(2, 2).Build()
+                                             }).Build()
+                        }).Build(),
+                    new ShipBuilder().WithName("firstShip")
+                                     .WithCells(new[]
+                                     {
+                                         new CellBuilder().WithCoordinates(2, 2).Build()
+                                     }).Build())
+            };
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithName("xxx")
+                                         .WithCells(new[]
+                                         {
+                                             new CellBuilder().WithCoordinates(2, 2).WithIsShot(true).Build(),
+                                             new CellBuilder().WithCoordinates(4, 2).WithIsShot(true).Build(),
+                                             new CellBuilder().WithCoordinates(3, 2).WithIsShot(true).Build()
+                                         }).Build(),
+                        new ShipBuilder().WithName("yyy")
+                                         .WithCells(new[]
+                                         {
+                                             new CellBuilder().WithCoordinates(24, 6).Build(),
+                                             new CellBuilder().WithCoordinates(24, 7).WithIsShot(true).Build(),
+                                         }).Build()
+                    }).Build(),
+                new CellBuilder().WithCoordinates(24, 6).Build(),
+                new GameMoveResult(
+                    new BoardBuilder()
+                        .WithShips(new[]
+                        {
+                            new ShipBuilder().WithName("xxx")
+                                             .WithCells(new[]
+                                             {
+                                                 new CellBuilder().WithCoordinates(2, 2).WithIsShot(true).Build(),
+                                                 new CellBuilder().WithCoordinates(4, 2).WithIsShot(true).Build(),
+                                                 new CellBuilder().WithCoordinates(3, 2).WithIsShot(true).Build()
+                                             }).Build(),
+                            new ShipBuilder().WithName("yyy")
+                                             .WithCells(new[]
+                                             {
+                                                 new CellBuilder().WithCoordinates(24, 6).Build(),
+                                                 new CellBuilder().WithCoordinates(24, 7).WithIsShot(true).Build(),
+                                             }).Build()
+                        }).Build(),
+                    new ShipBuilder().WithName("yyy")
+                                     .WithCells(new[]
+                                     {
+                                         new CellBuilder().WithCoordinates(24, 6).Build(),
+                                         new CellBuilder().WithCoordinates(24, 7).WithIsShot(true).Build(),
+                                     }).Build())
             };
         }
 
