83dff486c2c4fa02dd5f53d35c6954b7ff446513
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 20:11:11 +0100

    Added testcases to be implemented for Game

---
+++ CoreTests/TestData/Services/Game/ShotAtClassData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Model;
+using CoreTests.Utils;
+
+namespace CoreTests.TestData.Services.Game
+{
+    public class ShotAtClassData : IEnumerable<object[]>
+    {
+        private static Cell ShotCell() => new CellBuilder().WithIsShot(true).Build();
+
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            ShotCell()
+                        }).Build()
+                    }).Build()
+            };
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            ShotCell(),
+                            ShotCell()
+                        }).Build()
+                    }).Build()
+            };
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            ShotCell(),
+                            ShotCell()
+                        }).Build(),
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            ShotCell(),
+                            ShotCell(),
+                            ShotCell()
+                        }).Build()
+                    }).Build()
+            };
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
