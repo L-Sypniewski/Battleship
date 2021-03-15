1474de18ddac8d2a1b589b8adba959c417cbfd25
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 22:03:37 +0100

    Cell is compared only by its coordinates

--- Core/Model/Cell.cs
+++ Core/Model/Cell.cs
@@ -1,4 +1,12 @@
+using System;
+
 namespace Core.Model
 {
-    public sealed record Cell(int XCoordinate, int YCoordinate, bool IsShot);
+    public sealed record Cell(int XCoordinate, int YCoordinate, bool IsShot)
+    {
+        public override int GetHashCode() => HashCode.Combine(XCoordinate, YCoordinate);
+
+
+        public bool Equals(Cell? other) => XCoordinate.Equals(other?.XCoordinate) && YCoordinate.Equals(other.YCoordinate);
+    }
 }
