c26baa786ecd9eac00fe1eeaeae9445b575039ca
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 19:17:18 +0100

    Added Game stub

---
+++ Core/Services/Game.cs
@@ -0,0 +1,18 @@
+﻿using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class Game : IGame
+    {
+        public Board StartGame() => throw new System.NotImplementedException();
+
+
+        public void EndGame() => throw new System.NotImplementedException();
+
+
+        public bool IsFinished() => throw new System.NotImplementedException();
+
+
+        public GameMoveResult ShootAt(Cell cell) => throw new System.NotImplementedException();
+    }
+}
