969cd66d6fa0db880c2ecf1e6bc11fcd6d89ddad
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 10:21:34 +0100

    Implemented CellRandomizer

--- Core/Services/CellRandomizer.cs
+++ Core/Services/CellRandomizer.cs
@@ -1,9 +1,18 @@
-﻿using Core.Model;
+﻿using System;
+using Core.Model;
 
 namespace Core.Services
 {
     public sealed class CellRandomizer : ICellRandomizer
     {
-        public Cell GetCellWithin(BoardSize boardSize) => throw new System.NotImplementedException();
+        private readonly Random _random = new();
+
+
+        public Cell GetCellWithin(BoardSize boardSize)
+        {
+            var xCoordinate = _random.Next(0, boardSize.XSize + 1);
+            var yCoordinate = _random.Next(0, boardSize.YSize + 1);
+            return new Cell(xCoordinate, yCoordinate, false);
+        }
     }
 }
