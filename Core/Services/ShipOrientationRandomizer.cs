11bf9a39f776f8525bbe4af730ea26cd6c76c037
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 09:14:27 +0100

    Added ShipOrientationRandomizer stub

---
+++ Core/Services/ShipOrientationRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class ShipOrientationRandomizer : IShipOrientationRandomizer
+    {
+        private readonly Random _random = new Random();
+        private readonly ShipOrientation[] _shipOrientationValues = Enum.GetValues<ShipOrientation>();
+
+
+        public ShipOrientation GetOrientation() =>
+            ( ShipOrientation ) _shipOrientationValues.GetValue(_random.Next(_shipOrientationValues.Length))!;
+    }
+}
