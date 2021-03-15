b57c157b911f0d99b324f177bb77a4f2c28cd494
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 10:21:04 +0100

    Added BoardBuilder

---
+++ CoreTests/Utils/Builders/BoardBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Core.Model;
+
+namespace CoreTests.Utils
+{
+    public class BoardBuilder
+    {
+        private BoardSize _boardSize = new(0, 0);
+        private ImmutableArray<Ship> _ships = Enumerable.Empty<Ship>().ToImmutableArray();
+
+
+        public BoardBuilder WithSize(BoardSize size)
+        {
+            _boardSize = size;
+            return this;
+        }
+
+
+        public BoardBuilder WithShips(IEnumerable<Ship> ships)
+        {
+            _ships = ships.ToImmutableArray();
+            return this;
+        }
+
+
+        public Board Build()
+        {
+            return new(_boardSize, _ships);
+        }
+    }
+}
