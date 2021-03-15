081373a78e0dc913c105581a4edeafcd14d4c2df
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 17:36:38 +0100

    BoardInitializer refactor, fixed potential Exceptions thrown by Equals() in Ship

--- Core/Services/BoardInitializer.cs
+++ Core/Services/BoardInitializer.cs
@@ -19,7 +19,6 @@ namespace Core.Services
             _shipPositioner = shipPositioner;
             _cellVerifier = cellVerifier;
             _maxAttempts = Math.Max(1, maxAttempts);
-            ;
         }
 
 
@@ -32,7 +31,7 @@ namespace Core.Services
                 .SelectMany(config => Enumerable.Range(0, config.ShipsNumber).Select(_ => new {config.Name, config.ShipSize}));
 
             var ships = Enumerable.Empty<Ship>().ToImmutableArray();
-            var positions = Enumerable.Empty<Cell>().ToImmutableArray();
+
             foreach (var shipToCreate in shipsToCreate)
             {
                 var counter = 0;
@@ -41,13 +40,14 @@ namespace Core.Services
                     try
                     {
                         var newShipPosition = _shipPositioner.ShipPositionsFor(board, shipToCreate.ShipSize).ToImmutableArray();
-                        var updatedPositions = positions.AddRange(newShipPosition);
+                        var updatedPositions = ships.SelectMany(ship => ship.Cells).ToImmutableArray().AddRange(newShipPosition);
 
                         var doShipsIntersectEachOther = _cellVerifier.CellsIntersect(updatedPositions);
+                        var newShip = new Ship(shipToCreate.Name, newShipPosition);
+                        
                         if (!doShipsIntersectEachOther)
                         {
-                            ships = ships.Add(new Ship(shipToCreate.Name, newShipPosition));
-                            positions = updatedPositions;
+                            ships = ships.Add(newShip);
                             break;
                         }
 
@@ -67,6 +67,19 @@ namespace Core.Services
 
 
             return new Board(boardSize, ships);
+        }
+
+
+        private static Ship? CreateFor(string name, int size, Board board, IEnumerable<Ship> allCurrentlyCreatedShips,
+                                       IShipPositioner shipPositioner, ICellVerifier cellVerifier)
+        {
+            var newShipPosition = shipPositioner.ShipPositionsFor(board, size).ToImmutableArray();
+            var updatedPositions = allCurrentlyCreatedShips.SelectMany(s => s.Cells).ToImmutableArray().AddRange(newShipPosition);
+
+            var doShipsIntersectEachOther = cellVerifier.CellsIntersect(updatedPositions);
+            return !doShipsIntersectEachOther
+                ? new Ship(name, newShipPosition)
+                : null;
         }
     }
 }
