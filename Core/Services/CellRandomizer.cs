d0af3e5624a2bbea4f040faf942eab27b9c93a84
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 09:14:05 +0100

    Added CellRandomizer stub

---
+++ Core/Services/CellRandomizer.cs
@@ -0,0 +1,9 @@
+﻿using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class CellRandomizer : ICellRandomizer
+    {
+        public Cell GetCellWithin(BoardSize boardSize) => throw new System.NotImplementedException();
+    }
+}
---
+++ Core/Services/Interfaces/ICellRandomizer.cs
@@ -0,0 +1,9 @@
+﻿using Core.Model;
+
+namespace Core.Services
+{
+    public interface ICellRandomizer
+    {
+        Cell GetCellWithin(BoardSize boardSize);
+    }
+}
