7145ba15becb02b5fabdeffccaa0cf2a94a42da5
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 16:08:41 +0100

    Fixed BoardInitializer test for number of created Ships

---
+++ CoreTests/TestData/Services/BoardVerifier/BoardInitializer/NumberOfPlacedShipsClassData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Model;
+using CoreTests.Utils;
+
+namespace CoreTests.TestData.Services.BoardVerifier.BoardInitializer
+{
+    public class NumberOfPlacedShipsClassData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new HashSet<ShipConfiguration>(new[]
+                {
+                    new ShipConfigurationBuilder().WithShipSize(1).WithShipsNumber(2).Build(),
+                    new ShipConfigurationBuilder().WithShipSize(2).WithShipsNumber(4).Build(),
+                }),
+                6
+            };
+            yield return new object[]
+            {
+                new HashSet<ShipConfiguration>(new[]
+                {
+                    new ShipConfigurationBuilder().WithShipSize(4).WithShipsNumber(2).Build(),
+                    new ShipConfigurationBuilder().WithShipSize(1).WithShipsNumber(2).Build(),
+                }),
+                4
+            };
+            yield return new object[]
+            {
+                new HashSet<ShipConfiguration>(new[]
+                {
+                    new ShipConfigurationBuilder().WithShipSize(2).WithShipsNumber(5).Build()
+                }),
+                5
+            };
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
