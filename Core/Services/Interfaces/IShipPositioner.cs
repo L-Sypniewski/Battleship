bd0bcb3b6d6bfa104c64db95c07e4bb26f81e79f
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 10:25:47 +0100

    Implemented ShipPositioner - placing Ships horizontally

---
+++ Core/Services/Interfaces/IShipPositioner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Core.Model;
+
+namespace Core.Services
+{
+    public interface IShipPositioner
+    {
+        IReadOnlyCollection<Cell> ShipPositionsFor(Board board, int shipSize);
+    }
+}
---
+++ Core/Services/ShipPositioner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class ShipPositioner : IShipPositioner
+    {
+        private readonly IShipOrientationRandomizer _shipOrientationRandomizer;
+        private readonly ICellRandomizer _cellRandomizer;
+        private readonly IBoardVerifier _boardVerifier;
+
+
+        public ShipPositioner(IShipOrientationRandomizer shipOrientationRandomizer,
+                              ICellRandomizer cellRandomizer,
+                              IBoardVerifier boardVerifier)
+        {
+            _shipOrientationRandomizer = shipOrientationRandomizer;
+            _cellRandomizer = cellRandomizer;
+            _boardVerifier = boardVerifier;
+        }
+
+
+        public IReadOnlyCollection<Cell> ShipPositionsFor(Board board, int shipSize)
+        {
+            var firstCell = _cellRandomizer.GetCellWithin(board.Size);
+
+            var shipPositions = new List<Cell>(shipSize);
+            for (var i = firstCell.XCoordinate; i < firstCell.XCoordinate + shipSize; i++)
+            {
+                shipPositions.Add(new Cell(i, firstCell.YCoordinate, false));
+            }
+
+            return shipPositions;
+        }
+    }
+}
