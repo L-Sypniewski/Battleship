893d9c31f87b0068bedfbed6400c4ec8fcca12aa
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 12:05:00 +0100

    ShipPositioner creates throws exception if maximum number of attempts to create positions has been exceeded

--- CoreTests/Services/ShipPositionerTest.cs
+++ CoreTests/Services/ShipPositionerTest.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Core.Exceptions;
 using Core.Model;
 using Core.Services;
-using CoreTests.TestData.Services.CellVerifier;
 using CoreTests.TestData.Services.ShipPositioner;
 using CoreTests.Utils;
 using FluentAssertions;
@@ -12,9 +12,10 @@ using Xunit;
 
 namespace CoreTests.Services
 {
+    [SuppressMessage("ReSharper", "CoVariantArrayConversion")]
     public class ShipPositionerTest
     {
-        private readonly ShipPositioner _sut;
+        private ShipPositioner _sut;
 
         private readonly Mock<IShipOrientationRandomizer> _shipOrientationRandomizer = new();
         private readonly Mock<ICellRandomizer> _cellRandomizer = new();
@@ -23,70 +24,107 @@ namespace CoreTests.Services
 
         public ShipPositionerTest()
         {
-            _sut = new ShipPositioner(_shipOrientationRandomizer.Object, _cellRandomizer.Object, _boardVerifier.Object);
+            const int maxAttempts = 1;
+            _sut = CreateSut(maxAttempts);
         }
 
 
-           [Theory(DisplayName = "Ship are correctly placed in horizontal position")]
-           [ClassData(typeof(PositioningShipsHorizontallyClassData))]
-           public void Ship_are_correctly_placed_in_horizontal_position(Cell firstCell, int shipSize, Cell[] expectedCells)
-           {
-               _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
-                              .Returns(firstCell);
-               _boardVerifier.Setup(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
-                             .Returns(true);
-               _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
-                                         .Returns(ShipOrientation.Horizontal);
-   
-               var shipCells = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize);
-   
-   
-               shipCells.Should().BeEquivalentTo(expectedCells);
-           }
-   
-   
-           [Theory(DisplayName = "Ship are correctly placed in vertical position")]
-           [ClassData(typeof(PositioningShipsVerticallyClassData))]
-           public void Ship_are_correctly_placed_in_vertical_position(Cell firstCell, int shipSize, Cell[] expectedCells)
-           {
-               _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
-                              .Returns(firstCell);
-               _boardVerifier.Setup(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
-                             .Returns(true);
-               _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
-                                         .Returns(ShipOrientation.Vertical);
-   
-               var shipCells = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize);
-   
-   
-               shipCells.Should().BeEquivalentTo(expectedCells);
-           }
-   
-   
-           [Theory(DisplayName = "New position is not returned unless it is verified to be within Board bounds")]
-           [InlineData(ShipOrientation.Horizontal)]
-           [InlineData(ShipOrientation.Vertical)]
-           public void New_position_is_not_returned_unless_it_is_verified_to_be_within_Board_bounds(ShipOrientation shipOrientation)
-           {
-               var firstCell = new Cell(0, 0, false);
-               _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
-                              .Returns(firstCell);
-   
-               _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
-                                         .Returns(shipOrientation);
-   
-               const int numberOfFailedVerifications = 4;
-               _boardVerifier.SetupSequence(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(),It.IsAny<IEnumerable<Cell>>()))
-                             .Returns(false)
-                             .Returns(false)
-                             .Returns(false)
-                             .Returns(true);
-   
-               var _ = _sut.ShipPositionsFor(new BoardBuilder().WithSize(new BoardSize(1, 1)).Build(), 1);
-   
-   
-               _cellRandomizer.Verify(mock => mock.GetCellWithin(It.IsAny<BoardSize>()), Times.Exactly(numberOfFailedVerifications));
-               _shipOrientationRandomizer.Verify(mock => mock.GetOrientation(), Times.Exactly(numberOfFailedVerifications));
-           }
+        private ShipPositioner CreateSut(int maxAttemps) => new(
+            maxAttemps, _shipOrientationRandomizer.Object, _cellRandomizer.Object, _boardVerifier.Object);
+
+
+        [Theory(DisplayName = "Ship are correctly placed in horizontal position")]
+        [ClassData(typeof(PositioningShipsHorizontallyClassData))]
+        public void Ship_are_correctly_placed_in_horizontal_position(Cell firstCell, int shipSize, Cell[] expectedCells)
+        {
+            _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
+                           .Returns(firstCell);
+            _boardVerifier.Setup(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
+                          .Returns(true);
+            _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
+                                      .Returns(ShipOrientation.Horizontal);
+
+
+            var shipCells = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize);
+
+
+            shipCells.Should().BeEquivalentTo(expectedCells);
+        }
+
+
+        [Theory(DisplayName = "Ship are correctly placed in vertical position")]
+        [ClassData(typeof(PositioningShipsVerticallyClassData))]
+        public void Ship_are_correctly_placed_in_vertical_position(Cell firstCell, int shipSize, Cell[] expectedCells)
+        {
+            _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
+                           .Returns(firstCell);
+            _boardVerifier.Setup(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
+                          .Returns(true);
+            _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
+                                      .Returns(ShipOrientation.Vertical);
+
+
+            var shipCells = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize);
+
+
+            shipCells.Should().BeEquivalentTo(expectedCells);
+        }
+
+
+        [Theory(DisplayName = "New position is not returned unless it is verified to be within Board bounds")]
+        [InlineData(ShipOrientation.Horizontal)]
+        [InlineData(ShipOrientation.Vertical)]
+        public void New_position_is_not_returned_unless_it_is_verified_to_be_within_Board_bounds(ShipOrientation shipOrientation)
+        {
+            const int numberOfFailedVerifications = 3;
+            const int maxAttempts = numberOfFailedVerifications + 1;
+            _sut = CreateSut(maxAttempts);
+
+            var firstCell = new Cell(0, 0, false);
+            _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
+                           .Returns(firstCell);
+
+            _shipOrientationRandomizer.Setup(mock => mock.GetOrientation())
+                                      .Returns(shipOrientation);
+
+            _boardVerifier.SetupSequence(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
+                          .Returns(false)
+                          .Returns(false)
+                          .Returns(false)
+                          .Returns(true);
+
+
+            var _ = _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize: 1);
+
+
+            const int expectedNumberOfServiceCalls = numberOfFailedVerifications + 1;
+            _cellRandomizer.Verify(mock => mock.GetCellWithin(It.IsAny<BoardSize>()),
+                                   Times.Exactly(expectedNumberOfServiceCalls));
+            _shipOrientationRandomizer.Verify(mock => mock.GetOrientation(), Times.Exactly(expectedNumberOfServiceCalls));
+        }
+
+
+        [Theory(DisplayName = "Exception is thrown if could not create valid positions after defined number of max attempts")]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(5)]
+        public void Exception_is_thrown_if_could_not_create_valid_positions_after_defined_number_of_max_attempts(int maxAttempts)
+        {
+            _sut = CreateSut(maxAttempts);
+
+            var firstCell = new Cell(0, 0, false);
+            _cellRandomizer.Setup(mock => mock.GetCellWithin(It.IsAny<BoardSize>()))
+                           .Returns(firstCell);
+
+            _boardVerifier.Setup(mock => mock.CellsAreWithinBounds(It.IsAny<BoardSize>(), It.IsAny<IEnumerable<Cell>>()))
+                          .Returns(false);
+
+            
+            Action act = () => _sut.ShipPositionsFor(new BoardBuilder().Build(), shipSize: 1);
+
+
+            act.Should().Throw<CannotCreateShipPositionsException>();
+        }
     }
 }
