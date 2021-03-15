614ed8a8c14f4e1c2cd19e28cbcba71c5b55747f
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 17:04:01 +0100

    Added testcases for verifying service calls

--- CoreTests/Services/BoardInitializerTest.cs
+++ CoreTests/Services/BoardInitializerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Core.Model;
 using Core.Services;
 using CoreTests.TestData.Services.BoardVerifier.BoardInitializer;
+using CoreTests.Utils;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -11,15 +12,19 @@ namespace CoreTests.Services
 {
     public class BoardInitializerTest
     {
-        private readonly BoardInitializer _sut;
+        private BoardInitializer _sut;
         private readonly Mock<IShipPositioner> _shipPositioner = new();
         private readonly Mock<ICellVerifier> _cellVerifier = new();
 
 
         public BoardInitializerTest()
         {
-            _sut = new BoardInitializer(_shipPositioner.Object, _cellVerifier.Object, 10);
+            const int maxAttempts = 1;
+            _sut = CreateSut(maxAttempts);
         }
+
+
+        private BoardInitializer CreateSut(int maxAttempts) => new(_shipPositioner.Object, _cellVerifier.Object, maxAttempts);
 
 
         private void SetupShipPositionerToReturn(IReadOnlyCollection<Cell> cells)
@@ -41,9 +46,11 @@ namespace CoreTests.Services
         {
             SetupShipPositionerToReturn(Array.Empty<Cell>());
             SetupCellIntersectionVerifierToReturn(false);
-
             BoardSize boardSize = new(0, 0);
+
+
             var initializedBoard = _sut.InitializedBoard(boardSize, shipConfigurations);
+
 
             initializedBoard.Ships.Should().HaveCount(expectedNumberOfPlacedShips,
                                                       "number of placed ships should equal a sum of ships from shipConfigurations");
@@ -58,12 +65,65 @@ namespace CoreTests.Services
         {
             SetupShipPositionerToReturn(positionsCreatedByPositioner);
             SetupCellIntersectionVerifierToReturn(false);
-
             BoardSize boardSize = new(0, 0);
+
+
             var initializedBoard = _sut.InitializedBoard(boardSize, shipConfigurations);
+
 
             initializedBoard.Ships.Should().BeEquivalentTo(expectedCreatedShips,
                                                            "created Ships should have values assigned from ShipPositioner and ShipConfiguration");
+        }
+
+
+        [Theory(DisplayName = "Services are called once for each Ship if all verifications succeeded")]
+        [ClassData(typeof(NumberOfPlacedShipsClassData))]
+        public void Services_are_called_once_for_each_Ship_if_all_verifications_succeeded(
+            ISet<ShipConfiguration> shipConfigurations, int expectedNumberOfCalls)
+        {
+            SetupShipPositionerToReturn(Array.Empty<Cell>());
+            SetupCellIntersectionVerifierToReturn(false);
+
+            BoardSize boardSize = new(0, 0);
+
+
+            var _ = _sut.InitializedBoard(boardSize, shipConfigurations);
+
+
+            _shipPositioner.Verify(mock => mock.ShipPositionsFor(It.IsAny<Board>(), It.IsAny<int>()),
+                                   Times.Exactly(expectedNumberOfCalls));
+            _cellVerifier.Verify(mock => mock.CellsIntersect(It.IsAny<IEnumerable<Cell>>()),
+                                 Times.Exactly(expectedNumberOfCalls));
+        }
+
+
+        [Fact(DisplayName = "Board is not returned unless all verifications pass")]
+        public void Board_is_not_returned_unless_all_verifications_pass()
+        {
+            const int numberOfFailedVerifications = 4;
+            const int maxAttempts = numberOfFailedVerifications + 1;
+            _sut = CreateSut(maxAttempts);
+            
+            SetupShipPositionerToReturn(Array.Empty<Cell>());
+            _cellVerifier.SetupSequence(mock => mock.CellsIntersect(It.IsAny<IEnumerable<Cell>>()))
+                         .Returns(true)
+                         .Returns(true)
+                         .Returns(true)
+                         .Returns(true)
+                         .Returns(false);
+
+            var shipConfiguration = new ShipConfigurationBuilder().WithShipsNumber(1).Build();
+            BoardSize boardSize = new(0, 0);
+
+
+            var _ = _sut.InitializedBoard(boardSize, new HashSet<ShipConfiguration>(new[] {shipConfiguration}));
+
+
+            const int expectedNumberOfServiceCalls = numberOfFailedVerifications + 1;
+            _shipPositioner.Verify(mock => mock.ShipPositionsFor(It.IsAny<Board>(), It.IsAny<int>()),
+                                   Times.Exactly(expectedNumberOfServiceCalls));
+            _cellVerifier.Verify(mock => mock.CellsIntersect(It.IsAny<IEnumerable<Cell>>()),
+                                 Times.Exactly(expectedNumberOfServiceCalls));
         }
     }
 }
