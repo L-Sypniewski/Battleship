2e0ca6b5445d04461d8daf200c3846f1fb130c92
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 19:52:23 +0100

    Game - implemented checking if game is finished

---
+++ CoreTests/TestData/Services/Game/NotFinishedGameClassData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Model;
+using CoreTests.Utils;
+
+namespace CoreTests.TestData.Services.Game
+{
+    public class NotFinishedGameClassData : IEnumerable<object[]>
+    {
+        private static Cell ShotCell() => new CellBuilder().WithIsShot(true).Build();
+        private static Cell NotShotCell() => new CellBuilder().WithIsShot(false).Build();
+
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            NotShotCell()
+                        }).Build()
+                    }).Build()
+            };
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            NotShotCell(),
+                            ShotCell()
+                        }).Build()
+                    }).Build()
+            };
+            yield return new object[]
+            {
+                new BoardBuilder()
+                    .WithShips(new[]
+                    {
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            NotShotCell(),
+                            NotShotCell()
+                        }).Build(),
+                        new ShipBuilder().WithCells(new[]
+                        {
+                            ShotCell(),
+                            ShotCell(),
+                            ShotCell()
+                        }).Build()
+                    }).Build()
+            };
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
