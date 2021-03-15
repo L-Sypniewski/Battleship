06f2ff612e68db8c97968a454735c85fccb7ccf1
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-15 12:09:34 +0100

    Applied code cleanup to the whole solution

--- Core/Model/Ship.cs
+++ Core/Model/Ship.cs
@@ -7,16 +7,16 @@ namespace Core.Model
 {
     public sealed record Ship(string Name, IImmutableList<Cell> Cells)
     {
+        public bool Equals(Ship? other) => Name.Equals(other?.Name) ||
+                                           Cells.SequenceEqual(other?.Cells ?? new ImmutableArray<Cell>());
+
+
         private bool PrintMembers(StringBuilder builder)
         {
             builder.Append($"{nameof(Name)}: {Name},");
             builder.Append($"{nameof(Cells)}: {string.Join(',', Cells.Select(cell => cell.ToString()))}");
             return true;
         }
-
-
-        public bool Equals(Ship? other) => Name.Equals(other?.Name) ||
-                                           Cells.SequenceEqual(other?.Cells ?? new ImmutableArray<Cell>());
 
 
         public override int GetHashCode() => HashCode.Combine(Name, Cells);
