584a60cc92dc55fa1a7af96090af6ce860d86dcc
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-16 14:38:08 +0100

    Refactored Cell - restored default equality comparers, adjusted Game to the change

--- Core/Model/Cell.cs
+++ Core/Model/Cell.cs
@@ -2,9 +2,5 @@ using System;
 
 namespace Core.Model
 {
-    public sealed record Cell(int XCoordinate, int YCoordinate, bool IsShot)
-    {
-        public bool Equals(Cell? other) => XCoordinate.Equals(other?.XCoordinate) && YCoordinate.Equals(other.YCoordinate);
-        public override int GetHashCode() => HashCode.Combine(XCoordinate, YCoordinate);
-    }
+    public sealed record Cell(int XCoordinate, int YCoordinate, bool IsShot);
 }
