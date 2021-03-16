d5babc43bbae5b7f22de99994c1aacaf91fefdba
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-16 14:38:08 +0100

    Refactored Cell - restored default equality comparers, adjusted Game to the change

---
+++ CoreTests/TestData/Services/BoardVerifier/BoardInitializer/CreatingCellsWithoutShipsClassData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Model;
+using CoreTests.TestUtils;
+
+namespace CoreTests.TestData.Services.BoardVerifier.BoardInitializer
+{
+    public class CreatingCellsWithoutShipsClassData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new BoardSize(3, 3),
+                new[]
+                {
+                    new[]
+                    {
+                        new CellBuilder().WithCoordinates(0, 0).Build(),
+                        new CellBuilder().WithCoordinates(0, 1).Build()
+                    },
+                    new[]
+                    {
+                        new CellBuilder().WithCoordinates(1,2).Build(),
+                        new CellBuilder().WithCoordinates(2,2).Build()
+                    }
+                },
+                new[] {
+                    new CellBuilder().WithCoordinates(1,0).Build(),
+                    new CellBuilder().WithCoordinates(2,0).Build(),
+                    new CellBuilder().WithCoordinates(1,1).Build(),
+                    new CellBuilder().WithCoordinates(2,1).Build(),
+                    new CellBuilder().WithCoordinates(0,2).Build()
+                }
+            };
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
