6c81d541ba7a2a11f3add78824b31b32a48f0850
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-16 14:38:08 +0100

    Refactored Cell - restored default equality comparers, adjusted Game to the change

---
+++ Core/Model/CellIgnoringIsShotComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Model;
+
+namespace Core.Services
+{
+    public sealed class CellIgnoringIsShotComparer : IEqualityComparer<Cell>
+    {
+        public bool Equals(Cell? x, Cell? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x?.GetType() != y?.GetType()) return false;
+            return x?.XCoordinate == y?.XCoordinate && x?.YCoordinate == y?.YCoordinate;
+        }
+
+
+        public int GetHashCode(Cell obj)
+        {
+            return HashCode.Combine(obj.XCoordinate, obj.YCoordinate);
+        }
+    }
+}
--- Core/Services/CellVerifier.cs
+++ Core/Services/CellVerifier.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Core.Model;
 
@@ -10,26 +9,9 @@ namespace Core.Services
         public bool CellsIntersect(IEnumerable<Cell> cells)
         {
             var cellsArray = cells.ToArray();
-            var equalityComparer = new CellComparerIgnoringIsShot();
+            var equalityComparer = new CellIgnoringIsShotComparer();
             var cellsIntersect = cellsArray.Distinct(equalityComparer).Count() != cellsArray.Length;
             return cellsIntersect;
-        }
-
-
-        private sealed class CellComparerIgnoringIsShot : IEqualityComparer<Cell>
-        {
-            public bool Equals(Cell? x, Cell? y)
-            {
-                if (ReferenceEquals(x, y)) return true;
-                if (x?.GetType() != y?.GetType()) return false;
-                return x?.XCoordinate == y?.XCoordinate && x?.YCoordinate == y?.YCoordinate;
-            }
-
-
-            public int GetHashCode(Cell obj)
-            {
-                return HashCode.Combine(obj.XCoordinate, obj.YCoordinate);
-            }
         }
     }
 }
