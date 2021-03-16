50a6bc590ef062a513d04d255a7d5ce9ce85ee8f
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-16 14:10:36 +0100

    Added CellsWithoutShips property to Board

--- Core/Model/Board.cs
+++ Core/Model/Board.cs
@@ -5,34 +5,42 @@ using System.Text;
 
 namespace Core.Model
 {
-    public sealed record Board(BoardSize Size, IImmutableList<Ship> Ships)
+    public sealed record Board(BoardSize Size, IImmutableList<Ship> Ships,
+                               IImmutableList<Cell> CellsWithoutShips)
     {
         public bool Equals(Board? other) => Size.Equals(other?.Size) &&
-                                            Ships.SequenceEqual(other.Ships);
+                                            Ships.SequenceEqual(other.Ships) &&
+                                            CellsWithoutShips.SequenceEqual(other.CellsWithoutShips);
 
 
         private bool PrintMembers(StringBuilder builder)
         {
             builder.Append($"{nameof(BoardSize)}: {Size},");
             builder.Append($"{nameof(Ships)}: {string.Join(',', Ships.Select(ship => ship.ToString()))}");
+            builder.Append($"{nameof(CellsWithoutShips)}: {string.Join(',', CellsWithoutShips.Select(ship => ship.ToString()))}");
             return true;
         }
 
 
-        public override int GetHashCode() => HashCode.Combine(Size, Ships);
+        public override int GetHashCode() => HashCode.Combine(Size, Ships, CellsWithoutShips);
 
 
         public CellState CellState(Cell cellToCheck)
         {
-            var allCells = Ships.SelectMany(ship => ship.Cells).ToArray();
-            
-            var doesCellBelongsToShip = allCells.Contains(cellToCheck);
+            var doesCellBelongsToShip = DoesCellBelongToShip(Ships, cellToCheck);
             if (!doesCellBelongsToShip)
             {
-                return Model.CellState.Clear;
+                var cellWithoutShip = CellsWithoutShips.SingleOrDefault(cell => cell == cellToCheck);
+                if (cellWithoutShip is null)
+                {
+                    return Model.CellState.Clear;
+                }
+
+                return cellWithoutShip.IsShot ? Model.CellState.Miss : Model.CellState.Clear;
             }
 
-            var cellFromBoard = allCells.Single(cell => cell == cellToCheck);
+            var allShipCells = Ships.SelectMany(ship => ship.Cells).ToArray();
+            var cellFromBoard = allShipCells.Single(cell => cell == cellToCheck);
             if (!cellFromBoard.IsShot)
             {
                 return Model.CellState.Clear;
@@ -41,6 +49,13 @@ namespace Core.Model
             var shipForACell = Ships.Single(ship => ship.Cells.Contains(cellToCheck));
 
             return shipForACell.IsSunk ? Model.CellState.Sunk : Model.CellState.Hit;
+        }
+
+
+        private static bool DoesCellBelongToShip(IImmutableList<Ship> ships, Cell cellToCheck)
+        {
+            var allShipCells = ships.SelectMany(ship => ship.Cells).ToArray();
+            return allShipCells.Contains(cellToCheck);
         }
     }
 }
