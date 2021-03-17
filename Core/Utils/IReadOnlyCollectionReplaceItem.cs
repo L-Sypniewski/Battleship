f35c21d9a7d2495c6548d362be21605cc8c7cf0e
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-17 11:15:27 +0100

    Added extension method for replacing items in IReadOnlyCollection

---
+++ Core/Utils/IReadOnlyCollectionReplaceItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Core.Utils
+{
+    public static class ReadOnlyCollectionReplaceItem
+    {
+        public static IImmutableList<TItem> WithReplaced<TItem>(this IReadOnlyCollection<TItem> collection,
+                                                                TItem newItem, Predicate<TItem> predicate)
+        {
+            return ListWithReplacedItem(collection, newItem, predicate).ToImmutableList();
+        }
+
+
+        private static IList<TItem> ListWithReplacedItem<TItem>(IReadOnlyCollection<TItem> itemCollection,
+                                                                TItem newItem, Predicate<TItem> predicate)
+        {
+            var itemsList = itemCollection.ToList();
+            itemsList[itemsList.FindIndex(predicate)] = newItem;
+            return itemsList;
+        }
+    }
+}
