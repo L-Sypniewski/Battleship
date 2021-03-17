044d96b0234ffbbae3c55dda9dd4b27b82e9dbe2
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-17 11:50:49 +0100

    Added method for converting Excel column name to index

---
+++ Core/Utils/ColumnNames.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Core.Utils
+{
+    public static class ColumnNames
+    {
+        // Taken from https://stackoverflow.com/a/182924/8297552
+        public static string ToColumnName(this int number)
+        {
+            var dividend = number;
+            string columnName = string.Empty;
+
+            while (dividend > 0)
+            {
+                var modulo = ( dividend - 1 ) % 26;
+                columnName = Convert.ToChar(65 + modulo) + columnName;
+                dividend = ( dividend - modulo ) / 26;
+            }
+
+            return columnName;
+        }
+
+
+        // Taken from https://stackoverflow.com/a/848184/8297552
+        public static int ToColumnIndex(this string name)
+        {
+            var number = 0;
+            var pow = 1;
+            for (var i = name.Length - 1; i >= 0; i--)
+            {
+                number += ( name.ToUpper()[i] - 'A' + 1 ) * pow;
+                pow *= 26;
+            }
+
+            return number;
+        }
+    }
+}
