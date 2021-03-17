e435bb4f81b529b4f108b066b28a8b5105d53bc8
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-17 16:18:07 +0100

    Board throws custom Exception if trying to check state of null Cell

---
+++ Core/Exceptions/CannotCheckOutOfBoundsCellState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class CannotCheckOutOfBoundsCellState : Exception
+    {
+        public CannotCheckOutOfBoundsCellState()
+        {
+        }
+
+
+        public CannotCheckOutOfBoundsCellState(string message)
+            : base(message)
+        {
+        }
+
+
+        public CannotCheckOutOfBoundsCellState(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
