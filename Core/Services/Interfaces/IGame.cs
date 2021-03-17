00b35c5c0b945aa9bfd68fb15e49dfd71ffce6b6
Łukasz Sypniewski <l.sypniewski@gmail.com>
2021-03-17 13:01:26 +0100

    Removed redundant EndGame() from IGame

--- Core/Services/Interfaces/IGame.cs
+++ Core/Services/Interfaces/IGame.cs
@@ -5,7 +5,6 @@ namespace Core.Services
     public interface IGame
     {
         Board StartGame();
-        void EndGame();
         bool IsFinished(Board board);
         GameMoveResult ShootAt(Board board, Cell cell);
     }
